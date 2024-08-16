111ac3a5932f3d90e8c2ec4d7365f21dacf3feaa
miga-choi <miga.choi.1004@gmail.com>
2024-08-17 01:10:11 +0900

    create file "main.c"

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    // Load config files, if any.
+
+    // Run command loop.
+
+    // Perform any shutdown/cleanup.
+    return EXIT_SUCCESS;
+};
