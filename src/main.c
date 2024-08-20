f20ab8f286451ffa977e7ea4bb247c71760367bf
miga-choi <miga.choi.1004@gmail.com>
2024-08-20 22:47:47 +0900

    add builtin functions

--- src/main.c
+++ src/main.c
@@ -1,7 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
+/*
+ * Function Declarations for builtin shell commands:
+ */
+int sh_cd(char **args);
+
+int sh_help(char **args);
+
+int sh_exit(char **args0);
+
+/*
+ * List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+        "cd",
+        "help",
+        "exit"
+};
+
+int (*builtin_func[])(char **) = {
+        &sh_cd,
+        &sh_help,
+        &sh_exit
+};
+
+
+int sh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+ * Builtin function implementations.
+ */
+/**
+ * @brief Builtin command: change directory.
+ * @param args List of args. args[0] is "cd". args[1] is the directory.
+ * @return Always returns 1, to continue executing.
+ */
+int sh_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "sh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("sh");
+        }
+    }
+    return 1;
+}
+
+/**
+ * @brief Builtin command: print help.
+ * @param args List of args. Not examined.
+ * @return Always returns 1, to continue executing.
+ */
+int sh_help(char **args) {
+    int i;
+    printf("SH\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for (int i = 0; i < sh_num_builtins(); i++) {
+        printf("  %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+/**
+ * @brief Builtin command: exit.
+ * @param args List of args. Not examined.
+ * @return Always returns 0, to terminate execution.
+ */
+int sh_exit(char **args) {
+    return 0;
+}
 
 /**
  * @brief Launch a program and wait for it to terminate.
